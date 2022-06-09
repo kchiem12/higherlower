2b92aad0f7192740d71b1d43a87b98c4a6e8c405
ken <kchiem12@gmail.com>
2022-06-09 15:24:09 -0400

    Add input form for the player's number

--- common.js
+++ common.js
@@ -2,15 +2,68 @@ let counter = 0;
 
 const input = document.querySelector('.input');
 
-const lowerBound = document.querySelector('#lower-b');
-const upperBound = document.querySelector('#upper-b');
-const numRounds = document.querySelector('#rounds');
+const lowerB = document.querySelector('#lower-b');
+const upperB = document.querySelector('#upper-b');
+const rounds = document.querySelector('#rounds');
 const parameters = document.querySelector('.parameters');
+
+let lowerBound = 0;
+let upperBound = 0;
+let numRounds = 0;
+
+const playerInputForm = document.createElement('form');
+playerInputForm.classList.add('player-input');
+
+//this is to obtain the player's input
+const playerInput = document.createElement('p');
+playerInput.classList.add('game-input');
+
+const submission = document.createElement('p');
+submission.classList.add('game-input');
+const submitButton = document.createElement('input');
+submitButton.setAttribute('id', 'submit-button');
+submitButton.type = 'submit';
+submitButton.value = 'Submit your number!';
+submission.appendChild(submitButton);
+
+
+const textLabelPlayer = document.createElement('label');
+textLabelPlayer.htmlFor = 'player-num-input';
+
+const playerNumber = document.createElement('input');
+playerNumber.type = 'text';
+playerNumber.name = 'player-num-input';
+playerNumber.required = true;
+
+playerInput.appendChild(textLabelPlayer);
+playerInput.appendChild(playerNumber);
+playerInputForm.appendChild(playerInput);
+playerInputForm.appendChild(submission);
+
+
+
+
+//creates the counter
+const theCounter = document.createElement('p');
+theCounter.classList.add('counter');
+
+
+
+
 
 //function that begins the game once the startbutton is clicked
 function beginGame(e) {
-    console.log(`Hello the even that occurred is ${e}`);
+    lowerBound = Number(lowerB.value);
+    upperBound = Number(upperB.value);
+    numRounds = Number(rounds.value);
+    input.removeChild(parameters);
+    input.appendChild(playerInputForm);
+    console.log(`lowerBound = ${lowerBound}, upperBound = ${upperBound}, numRounds = ${numRounds}`);
+    textLabelPlayer.textContent = `Input your number (any number from ${lowerBound} to ${upperBound}): `;
+
     e.preventDefault();
+
+
 }
 
 const startButton = document.querySelector('#start-button');
